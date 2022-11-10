db65f788aeee7fd579cac1186313617f2c3dde9b
Ankeshkewat <ankeshkewat966@gmail.com>
2022-11-11 01:16:06 +0530

    Checkout page done

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,4 +1,5 @@
-
+import {footer} from '../Components/footer.js'
+document.getElementById('ak-footer').innerHTML=footer()
 let proceed_btn=document.getElementById('ak-place-order-btn')
 proceed_btn.onclick=()=>{
     UserDetails()
@@ -11,8 +12,12 @@ const UserDetails=()=>{
          let city=document.getElementById('ak-city').value
          let pin_code=document.getElementById('ak-pin-code').value
          let phone_number=document.getElementById('ak-phone').value
-
-        if(first_name!=''&&last_name!=''&&state!=''&&house_no!=''&&pin_code!=""&&city!=''&&phone_number!=''){
+         let cart_number=document.getElementById('ak-cart-number').value;
+         let account_holder=document.getElementById('ak-cart-holder').value
+         let exp_monnth=document.getElementById('ak-cart-exp-month').value
+         let exp_year=document.getElementById('ak-cart-exp-year').value
+         let cvv=document.getElementById('ak-cart-cvv').value
+        if(first_name!=''&&last_name!=''&&state!=''&&house_no!=''&&pin_code!=""&&city!=''&&phone_number!=''&&cart_number!=''&&account_holder!=''&&exp_monnth!=""&&exp_year!=""&&cvv!=""){
             alert('Payment Succesful')
         }else{
             alert('Fill all details')
